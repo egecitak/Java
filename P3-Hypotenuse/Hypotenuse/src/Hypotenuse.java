9e95fb423d7bb64a37df0c527c11ccdab71176fb
egecitak <egecitak01@gmail.com>
2022-08-08 12:50:15 +0300

    Hypotenuse practise is done with image

--- P3-Hypotenuse/Hypotenuse/src/Hypotenuse.java
+++ P3-Hypotenuse/Hypotenuse/src/Hypotenuse.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Hypotenuse {
+import java.util.Scanner;
+
+public class Hypotenuse {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter one of the orthogonal sides: ");
+        int firstSide = input.nextInt();
+        System.out.print("Enter the other orthogonal side: ");
+        int secondSide = input.nextInt();
+
+        double squareSum = Math.pow(firstSide, 2) + Math.pow(secondSide, 2);
+        double hypotenuse = Math.sqrt(squareSum);
+
+        System.out.println("Hypotenuse: " + hypotenuse);
+        System.out.println("Circumference: " + (firstSide + secondSide + hypotenuse));
+        System.out.println("Area: " + ((firstSide * secondSide) / 2));
+    }
 }
