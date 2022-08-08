c8fd27ed25dfd08114c6fdc011de3bd60bffd94b
egecitak <egecitak01@gmail.com>
2022-08-08 23:38:22 +0300

    Body mass index project is done with image

--- HW1-BodyMassIndex/BodyMassIndex/src/BodyMassIndex.java
+++ HW1-BodyMassIndex/BodyMassIndex/src/BodyMassIndex.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class BodyMassIndex {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Please enter your height in meters with a comma: ");
+        double height = input.nextDouble();
+        System.out.print("Please enter your weight with a comma: ");
+        double weight = input.nextDouble();
+        double index = weight / Math.pow(height, 2);
+
+        System.out.println("Your body-mass index: " + index);
     }
 }
