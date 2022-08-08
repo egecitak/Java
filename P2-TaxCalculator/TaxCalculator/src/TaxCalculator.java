d7f6d7ba7a6bbb2105e9fdf28ec5e7bb69ef9a92
egecitak <egecitak01@gmail.com>
2022-08-08 08:33:38 +0300

    last

--- P2-TaxCalculator/TaxCalculator/src/TaxCalculator.java
+++ P2-TaxCalculator/TaxCalculator/src/TaxCalculator.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class TaxCalculator {
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter price: ");
+        Scanner input = new Scanner(System.in);
+
+        double price = input.nextDouble();
+        double tax = (price * 18) / 100.0;
+        double total = tax + price;
+
+        System.out.println("Without tax: " + price);
+        System.out.println("Tax: " + tax);
+        System.out.println("Total Price: " + total);
+
+    }
 }
