5c5c6847b5f25f6cf9cf0963d6f4fe103e3825c0
egecitak <egecitak01@gmail.com>
2022-08-08 13:06:06 +0300

    Cab fare practise is done with image

--- P4-CabFare/CabFare/src/CabFare.java
+++ P4-CabFare/CabFare/src/CabFare.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 public class CabFare {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How far away is your destination in kms?");
+        double distance = input.nextDouble();
+
+        double fare = ((distance * 2.20) + 10 < 20) ? 20 : ((distance * 2.20) + 10);
+        System.out.println("Your fare: " + fare);
     }
 }
