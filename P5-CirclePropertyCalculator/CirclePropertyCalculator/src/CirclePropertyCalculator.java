7e10735099e9cc195bc699aff8d765811a5d74fc
egecitak <egecitak01@gmail.com>
2022-08-08 23:25:35 +0300

    Circle property calculator is done with images

--- P5-CirclePropertyCalculator/CirclePropertyCalculator/src/CirclePropertyCalculator.java
+++ P5-CirclePropertyCalculator/CirclePropertyCalculator/src/CirclePropertyCalculator.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class CirclePropertyCalculator {
     public static void main(String[] args) {
+        final double PI = 3.14;
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Please enter the radius: ");
+        double rad = input.nextDouble();
+        System.out.print("Please enter the angle (360 for full circle): ");
+        double ang = input.nextDouble();
+
+        double area = (PI * Math.pow(rad, 2)) * (ang / 360);
+        double circumference = 2 * PI * rad * (ang / 360);
+
+        System.out.println("Area: " + area);
+        System.out.println("Circumference: " + circumference);
     }
 }
