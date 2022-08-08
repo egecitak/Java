fd3dd5ff73d27a083c43bcf7db2337e559165f60
egecitak <egecitak01@gmail.com>
2022-08-08 08:19:28 +0300

    last

--- P1-Grader/Grader/src/Grader.java
+++ P1-Grader/Grader/src/Grader.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Grader {
+import java.util.Scanner;
+
+public class Grader {
+    public static void main(String[] args) {
+
+        int math, phys, chem, turk, hist, mus;
+
+        Scanner input = new Scanner(System.in);
+
+        //User input
+        System.out.print("Enter math grade: ");
+        math = input.nextInt();
+
+        System.out.print("Enter phys grade: ");
+        phys = input.nextInt();
+
+        System.out.print("Enter chem grade: ");
+        chem = input.nextInt();
+
+        System.out.print("Enter turk grade: ");
+        turk = input.nextInt();
+
+        System.out.print("Enter hist grade: ");
+        hist = input.nextInt();
+
+        System.out.print("Enter mus grade: ");
+        mus = input.nextInt();
+
+        double avg = (math + phys + chem + turk + hist + mus) / 6.0;
+        System.out.println("Your average: " + avg);
+
+        String result;
+
+        result = (avg >= 60) ? "Pass" : "Fail";
+        System.out.println(result);
+
+    }
 }
