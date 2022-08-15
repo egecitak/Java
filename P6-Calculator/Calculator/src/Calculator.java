6c84ad829ab9a2c4c0d3dd02957ccbbb7a0d4b8d
egecitak <egecitak01@gmail.com>
2022-08-15 13:02:12 +0300

    Calculator project is done with image

--- P6-Calculator/Calculator/src/Calculator.java
+++ P6-Calculator/Calculator/src/Calculator.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
 public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+
+        //nextInt leaves the new line character
+        input.nextLine();
+
+        System.out.print("Choose the operation (+, -, /, *): ");
+        String op = input.nextLine();
+
+        switch (op) {
+            case "+":
+                System.out.println(num1 + num2);
+                break;
+            case "-":
+                System.out.println(num1 - num2);
+                break;
+            case "*":
+                System.out.println(num1 * num2);
+                break;
+            case "/":
+                if (num2 != 0)
+                {
+                    System.out.println((double) num1 / num2);
+                } else {
+                    System.out.println("Division by zero");
+                }
+                break;
+            default:
+                System.out.println("Choose a correct operation");
+        }
+    }
 }
