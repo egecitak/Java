ce4db2d6149c86f271ab38a48f95d5519d415ed6
egecitak <egecitak01@gmail.com>
2022-09-08 23:55:49 +0300

    Activity based on temperature project is done

--- P9-ActivityBasedOnTemp/ActivityBasedOnTemp/src/ActivityBasedOnTemp.java
+++ P9-ActivityBasedOnTemp/ActivityBasedOnTemp/src/ActivityBasedOnTemp.java
@@ -4,5 +4,23 @@ public class ActivityBasedOnTemp {
 
     public static void main(String[] args) {
 
+        int temp;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the temperature: ");
+        temp = input.nextInt();
+
+        if (temp < 5 ) {
+            System.out.println("Go skiing.");
+        } else if (temp >= 5 && temp < 10) {
+            System.out.println("Go to a cinema.");
+        } else if (temp >= 10 && temp < 15) {
+            System.out.println("Go to a cinema or picnic.");
+        } else if (temp >= 15 && temp < 25) {
+            System.out.println("Go to a picnic.");
+        } else if (temp >= 25) {
+            System.out.println("Go swimming.");
+        }
+
     }
 }
